57f8bb9cb95a22b4fa66ac9bbd90b29b1f277f84
Celal <cllakyz@hotmail.com>
2020-04-23 02:11:22 +0300

    socket.io redis adapter kurulumu

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,9 +1,16 @@
 const socketio = require('socket.io');
+const redisAdapter = require('socket.io-redis');
+
 const io = socketio();
 
 const socketApi = {
     io,
 };
+
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URL,
+    port: process.env.REDIS_PORT
+}));
 
 io.on('connection', socket => {
     console.log('a user logged in');
