5da71e20826a20a10a6410486bf6abb3c2c2a437
Celal <cllakyz@hotmail.com>
2020-04-22 13:38:58 +0300

    dotenv install

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
